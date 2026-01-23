0d84f9102c0c6e1b0718c6cfd276a527f851abc1
kortecx <jayachandra.s.reddy@gmail.com>
2026-01-23 15:30:07 +0530

    ruva CLI configured

--- src/ruva/__init__.py
+++ src/ruva/__init__.py
@@ -1,2 +1,1 @@
-def hello() -> str:
-    return "Hello from ruva!"
+__all__ = ["__version__"]
---
+++ src/ruva/cli/entrypoint.py
@@ -0,0 +1,9 @@
+import click
+
+@click.group(help="Ruva CLI - Executable Intelligence CLI")
+def ruva():
+    pass
+
+
+def main():
+    ruva()  # pylint: disable=no-value-for-parameter
