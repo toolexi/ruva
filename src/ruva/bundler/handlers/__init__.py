24cefc4285774669c08f296a1b1a1ef3d3a3cced
kortecx <jayachandra.s.reddy@gmail.com>
2026-01-25 22:36:00 +0530

    container management, logging and restructured.
    restructured project to move llm and data and training functionality to other repo.

--- src/ruva/bundler/handlers/__init__.py
+++ src/ruva/bundler/handlers/__init__.py
@@ -1,3 +1,3 @@
-from ._image import ImageHandler
-from ._container import ContainerHandler
-from ._resource_handler import DockerResourceHandler
+from ._image import ImageHandler as ImageHandler
+from ._container import ContainerHandler as ContainerHandler
+from ._resource_handler import DockerResourceHandler as DockerResourceHandler
--- src/ruva/bundler/handlers/_resource_handler.py
+++ src/ruva/bundler/handlers/_resource_handler.py
@@ -1,43 +1,49 @@
 from dataclasses import dataclass
 from pathlib import Path
-from datetime import date,time
-import os
+from datetime import date
 import logging
 from typing import Optional
-from time import gmtime, strftime
+from time import strftime
+
 
 @dataclass(frozen=True)
 class DockerResourceHandler:
 
-    def _setup(self):
-        main_dir = Path.cwd()
-        logs_path = Path.cwd() / "logs"
-        today_dir = logs_path / date.today().isoformat()
+    def _setup(self, asset_type: str):
+        today_dir = Path.cwd() / "logs" / date.today().isoformat() / asset_type
         today_dir.mkdir(exist_ok=True, parents=True)
-        return logs_path, main_dir, today_dir
+        return today_dir
 
-    def persist_image_logs(self, image_name:str, stream: Optional[str]="", errors: Optional[str]=""):
-        logs_path, main_dir, today_dir = self._setup()
-        if (today_dir/f"{image_name}.txt").is_file() or (today_dir/f"{image_name}_errors.txt").is_file():
+    def persist_image_logs(
+        self,
+        asset_type: str,
+        image_name: Optional[str] = None,
+        container_name: Optional[str] = None,
+        stream: Optional[str] = "",
+        errors: Optional[str] = "",
+    ):
+        asset_picker = {"image": image_name, "container": container_name}
+        file_name = asset_picker[asset_type]
+        today_dir = self._setup(asset_type)
+        if (today_dir / f"{file_name}.txt").is_file() or (
+            today_dir / f"{file_name}_errors.txt"
+        ).is_file():
             logging.info("File exists, appending new logs to the file")
             if stream:
-                with open(today_dir/f"{image_name}.txt", "a") as logs_file:
-                    logs_file.write(strftime("%Y-%m-%d %H:%M:%S ", gmtime()) + stream + "\n")
+                with open(today_dir / f"{file_name}.txt", "a") as logs_file:
+                    logs_file.write(strftime("%Y-%m-%d %H:%M:%S ") + stream + "\n")
                     logs_file.close()
             elif errors:
-                with open(today_dir/f"{image_name}_errors.txt", "a") as logs_file:
-                    logs_file.write(strftime("%Y-%m-%d %H:%M:%S ", gmtime()) + errors + "\n")
+                with open(today_dir / f"{file_name}_errors.txt", "a") as logs_file:
+                    logs_file.write(strftime("%Y-%m-%d %H:%M:%S ") + errors + "\n")
                     logs_file.close()
         else:
             logging.info("Writing logs stream to the file")
             if stream:
-                with open(today_dir/f"{image_name}.txt", "a") as logs_file:
-                    logs_file.write(strftime("%Y-%m-%d %H:%M:%S ", gmtime()) + stream + "\n")
+                with open(today_dir / f"{file_name}.txt", "a") as logs_file:
+                    logs_file.write(strftime("%Y-%m-%d %H:%M:%S ") + stream + "\n")
                     logs_file.close()
             elif errors:
-                with open(today_dir/f"{image_name}_errors.txt", "a") as logs_file:
-                    logs_file.write(strftime("%Y-%m-%d %H:%M:%S ", gmtime()) + errors + "\n")
+                with open(today_dir / f"{file_name}_errors.txt", "a") as logs_file:
+                    logs_file.write(strftime("%Y-%m-%d %H:%M:%S ") + errors + "\n")
                     logs_file.close()
-
-
-
